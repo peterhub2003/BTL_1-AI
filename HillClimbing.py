1e32dd69ac6a1108cbdbd2b650f6fffe8ca154c8
KhoaTranAnh12 <131005608+KhoaTranAnh12@users.noreply.github.com>
2023-10-29 18:39:58 +0700

    HillClimbing.py kèm chú thích

--- HillClimbing.py
+++ HillClimbing.py
@@ -2,36 +2,37 @@
 import random
 from collections import Counter
 
-def initiate(N):
+def initiate(N):#Khởi tạo
     queens = random.sample(range(N),N)
     return queens
 
-def swap(queens,N,x1,x2):
+def swap(queens,N,x1,x2):#Hàm Swap, Thường gặp rất nhiều ở các bài toán liên quan tới mảng.
     temp = queens[x1]
     queens[x1] = queens[x2]
     queens[x2] = temp
 
-def heuristic(queens,N):
+def heuristic(queens,N):#Tính Heuristic bằng cách đếm số queens ở đường chéo.
         result=0
         [mainDiaCount,secDiaCount] = [Counter() for i in range(2)]
         for i in range(N):
-            mainDiaCount[queens[i]-i] += 1
-            secDiaCount[queens[i]+i] +=1
+            mainDiaCount[queens[i]-i] += 1 #Các queens ở cùng đường chéo chính sẽ có toạ độ dòng - toạ độ cột bằng nhau.
+            secDiaCount[queens[i]+i] +=1 #Các queens ở cùng đường chéo phụ sẽ có toạ độ dòng + toạ độ cột bằng nhau.
         for count in [mainDiaCount,secDiaCount]:
             for i in count:
-                result += count[i]*(count[i]-1)//2
+                result += count[i]*(count[i]-1)//2 #Gom lại các cặp queens cùng đường chéo, tính theo nguyên tắc bắt tay.
         return result
 def hC(N):
+    #Khởi tạo và đếm Heuristic ở queens khởi tạo.
     q=initiate(N)
     h=heuristic(q,N)
-    found = True
+    found = True #Đưa found = True để tránh việc found = False làm phá vỡ vòng lặp.
     while(1):
-        if h == 0:
+        if h == 0:#Lúc này là đã tìm được đáp án, return True.
             return q,True
-        if found == False:
+        if found == False:#Lúc này đã xác định rơi vào tối ưu cục bộ, return False. Cho dù True hay False đều return ra Queens tìm được.
             return q,False
         found = False
-        for i in random.sample(range(N), N):
+        for i in random.sample(range(N), N):#Swap random 2 chỗ với nhau.
             if found == True:
                 break
             for j in random.sample(range(N), N):
@@ -44,13 +45,15 @@ def hC(N):
                     found = True
                     q=newQ
                     h=newH
-                    break
-def randRestart(N,max_times=1000):
+                    break #Lưu ý break này chỉ break vòng lặp 2, để break toàn bộ ta cần biến boolean là found.
+def randRestart(N,max_times=1000): #Hàm random restart, chỉ là chạy nhiều lần N-Queens đến khi tìm được lời giải thôi.
     for i in range(max_times):
         hillClimb = hC(N)
         if hillClimb[1] == True:
             return hillClimb[0],i+1
     assert "Can't find the final result with " + str(max_times) + " steps"
+#Hàm để tính Hit rate, vì hàm hill climbing mang tính chất ngẫu nhiên nên Hit rate thường sẽ chênh nhau
+#Max_times càng ít hit rate chênh càng nhiều.
 def CalcHitRate(N,max_times=1000):
     hit = 0
     for i in range(max_times):
@@ -59,4 +62,3 @@ def CalcHitRate(N,max_times=1000):
             hit +=1
     print(res[0])
     print("Hit rate with n = : " + str(N) + " is: " + str(hit/max_times))    
-print(randRestart(7))
